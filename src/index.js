27f64df7f2e45ce90b270f95b39f55d53a6dd50f
Timur Karpenok <nbvehbq@gmail.com>
2017-05-11 00:10:13 +0300

    in progress

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 import path from 'path';
 import url from 'url';
 import fs from 'mz/fs';
+import cheerio from 'cheerio';
 
 import axios from './lib/axios';
 
-const buildFilenameFromUrl = (request) => {
+const buildPathNameFromUrl = (request, ext = '.html') => {
   const urlObject = url.parse(request, true);
   return `${[urlObject.host, urlObject.path]
     .join('')
-    .replace(/\W+/g, '-')}.html`;
+    .replace(/\W+/g, '-')}${ext}`;
 };
 
-export default (request, output = '.') => {
-  const filename = path.resolve(output, buildFilenameFromUrl(request));
+const getLocalUrls = (data) => {
+  const requestObects = [
+    { name: 'link', href: 'href' },
+    { name: 'img', href: 'src' },
+    { name: 'script', href: 'src' },
+  ];
+  const $ = cheerio.load(data);
 
-  return axios.get(request)
-    .then(response => fs.writeFile(filename, response.data));
+  return requestObects.reduce((acc, item) => {
+    const links = $(item.name).map((i, el) => $(el).attr(item.href)).get()
+      .filter((link) => {
+        const urlObject = url.parse(link, true);
+        return !urlObject.protocol;
+      });
+    return [...acc, ...links];
+  }, []);
+};
+
+const replaseUrlsWithLocalPath = data => data;
+
+export default (request, output = '.') => {
+  const filename = path.resolve(output, buildPathNameFromUrl(request));
+  const foldername = path.resolve(output, buildPathNameFromUrl(request, '_files'));
+
+  return fs.mkdir(foldername)
+    .then(() => axios.get(request))
+    .then((response) => {
+      const localUrls = getLocalUrls(response.data);
+      console.log(localUrls);
+      const newContent = replaseUrlsWithLocalPath(response.data);
+      const rootPage = fs.writeFile(filename, newContent);
+    });
 };
